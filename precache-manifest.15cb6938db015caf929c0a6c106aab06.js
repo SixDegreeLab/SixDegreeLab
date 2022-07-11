cd70d6e8569de7c0fd523d68eaf3482707d6d4cf
0xbitfly <0xbitfly@gmail.com>
2022-07-11 11:27:14 +0800

    Updates

---
+++ precache-manifest.15cb6938db015caf929c0a6c106aab06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56760f14bae2279f12833985ce13a842",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "cbfa3a37c9b603870c24",
+    "url": "/website/static/css/main.ab588325.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/website/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/website/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfa3a37c9b603870c24",
+    "url": "/website/static/js/main.7caccb42.chunk.js"
+  },
+  {
+    "revision": "f7ba02939af9c7e088f6",
+    "url": "/website/static/js/runtime-main.d57c616e.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/website/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/website/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.bf029c78a315764d5eba7e41ed596008.js"
+  "/website/precache-manifest.15cb6938db015caf929c0a6c106aab06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
