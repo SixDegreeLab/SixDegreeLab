c98f61131ef2a6c28ba28b8795b5bd6f4a894a04
0xbitfly <0xbitfly@gmail.com>
2022-09-01 19:42:19 +0800

    fix miss folder

---
+++ website/precache-manifest.55a135179799398a5cb820157e4cac3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf757888202306496311e344935c911",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "b238d01f666cbc7f1682",
+    "url": "/website/static/css/main.ab588325.chunk.css"
+  },
+  {
+    "revision": "2d61e1578c36f71acc84",
+    "url": "/website/static/js/2.0baf4c78.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/website/static/js/2.0baf4c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b238d01f666cbc7f1682",
+    "url": "/website/static/js/main.2c0e196d.chunk.js"
+  },
+  {
+    "revision": "f7ba02939af9c7e088f6",
+    "url": "/website/static/js/runtime-main.d57c616e.js"
+  },
+  {
+    "revision": "4adee87f5000ccb28f84bd506f8ea677",
+    "url": "/website/static/media/hashirshoaeb.4adee87f.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/website/static/media/resume.9d3da0e2.pdf"
+  }
+]);
