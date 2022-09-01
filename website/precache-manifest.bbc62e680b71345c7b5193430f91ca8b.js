8ec26e04d500b0ba087b9bccfaadbe7bedbfea04
0xbitfly <0xbitfly@gmail.com>
2022-09-01 20:16:21 +0800

    website 2

---
+++ website/precache-manifest.bbc62e680b71345c7b5193430f91ca8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb905ca3e033205505eb004463f98ed",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "49fcd1692802cef51bcf",
+    "url": "/website/static/css/main.ab588325.chunk.css"
+  },
+  {
+    "revision": "2d61e1578c36f71acc84",
+    "url": "/website/static/js/2.0baf4c78.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/website/static/js/2.0baf4c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fcd1692802cef51bcf",
+    "url": "/website/static/js/main.370b793f.chunk.js"
+  },
+  {
+    "revision": "f7ba02939af9c7e088f6",
+    "url": "/website/static/js/runtime-main.d57c616e.js"
+  },
+  {
+    "revision": "4adee87f5000ccb28f84bd506f8ea677",
+    "url": "/website/static/media/hashirshoaeb.4adee87f.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/website/static/media/resume.9d3da0e2.pdf"
+  }
+]);
